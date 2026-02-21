6fed18e0fc834ee6de27e74367be75bce4f6773a
Selvakumar <selvakumarsk2004a@gmail.com>
2026-02-21 15:58:38 +0530

    fix: rename user table to users to avoid postgres reserved word

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -5,14 +5,16 @@ import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 }
